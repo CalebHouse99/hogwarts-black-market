ba92d3300ea068ef4e45976fe9bc8fff8bc06d4b
unknown <calebhouse99@gmail.com>
2023-04-20 21:16:28 -0500

    added components for cart

---
+++ src/components/CartBtn.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import './CartBtn.css'
+
+function CartBtn() {
+  return (
+    <div className='CartBtn'>
+      <button>View Cart</button>
+    </div>
+  )
+}
+
+export default CartBtn
---
+++ src/components/Checkout.js
@@ -0,0 +1,10 @@
+import React from 'react'
+import './Checkout.css'
+
+function Checkout() {
+  return (
+    <div className='Checkout'>Checkout</div>
+  )
+}
+
+export default Checkout
