c5e326e3d4f5c2879bc301b3620b291b9badbabc
virtualblakewillis <bewillis101@gmail.com>
2023-04-24 09:58:39 -0600

    Homepage CSS tweaks

--- src/components/Tier.js
+++ src/components/Tier.js
@@ -3,11 +3,13 @@ import './Tier.css'
 function Tier({ title, tierPrice, isDarkMode }) {
   return (
     <div className={`Tier ${isDarkMode ? 'dark-mode' : ''}`}>
-      <img alt="Tier thumbnail"></img>
-      <h2>{title}</h2>
-      <a href="/signup">
-        <button>{tierPrice}</button>  
-      </a>
+      <img alt="Tier thumbnail" src='https://i.imgur.com/3ZQZ7Zm.png'></img>
+      <div className="TierContent">
+        <h2>{title}</h2>
+        <a href="/signup">
+          <button>{tierPrice}</button>  
+        </a>
+      </div>
     </div>
   );
 }
